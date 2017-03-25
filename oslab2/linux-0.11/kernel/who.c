69f02d70b511de8d87320226b045e55d519814c7
Your Name <you@example.com>
2017-03-25 01:11:21 +0000

    20170325

--- oslab2/linux-0.11/kernel/who.c
+++ oslab2/linux-0.11/kernel/who.c
@@ -1,24 +1,24 @@
-#define __LIBRARY__
-#include <unistd.h>
 #include <errno.h>
+#include <unistd.h>
 #include <asm/segment.h>
 char sys_name[25]={0};
 
 int sys_iam(const char * name){
+    int i=0;
     int count=0;
     while(*(name+count)!='\0'){
         count++;
     }
     if(count>23){
-        errno=ENIVAL;
-	printk("string is too long");
+        errno=EINVAL;
+	//printk("string is too long");
         return -1;
     }
     else{
-        for(int i=0;i<=count;i++){
+        for(i=0;i<=count;i++){
             sys_name[i]=*(name+i);
         }
-	printk("copy finished");
+	//printk("copy finished");
         return count;
     }
 }
@@ -26,18 +26,18 @@ int sys_iam(const char * name){
 int sys_whoami(char* name, unsigned int size){
     int i=0;
     while(sys_name[i]!='\0'){
-        i++
+        i++;
     }
     if(size<i){
         errno=-1;
-	printk("size is not enough");
+	//printk("size is not enough");
         return -1;
     }
     else{
         for(i=0;i<=size;i++){
             *(name+i)=sys_name[i];
         }
-	printk("copy finished");
+	//printk("copy finished");
         return size;
     }
 }
